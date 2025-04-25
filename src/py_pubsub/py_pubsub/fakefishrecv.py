aaf6441840f13689a7572c8c0b3dbfa650f3bb31
Ike <ikealafita@gmail.com>
2025-04-25 11:56:47 -0400

    Update fakefishrecv.py

--- src/py_pubsub/py_pubsub/fakefishrecv.py
+++ src/py_pubsub/py_pubsub/fakefishrecv.py
@@ -2,6 +2,12 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
 
+# Authors: Ike Alafita & Jacob Herrema
+# Michigan Technological University 
+# Open Source Hardware Enterprise
+# Open Source Thunniform Robot (fish)
+# This program is used to run the fish in a wired configuration over an ssh connection
+
 COMMAND_LIST = {  # List of commands mapped to 2-bit values
     "S": "00",
     "D": "01",
@@ -9,7 +15,7 @@ COMMAND_LIST = {  # List of commands mapped to 2-bit values
     "L": "10",
     "C": "11" 
 }
-NUMBER_LIST = {
+NUMBER_LIST = { # Basic hex number for fast string conversion
     "0": "0000", "1": "0001", "2": "0010", "3": "0011",
     "4": "0100", "5": "0101", "6": "0110", "7": "0111",
     "8": "1000", "9": "1001", "A": "1010", "B": "1011",
@@ -17,36 +23,35 @@ NUMBER_LIST = {
 }
 
 class fakefishrecv(Node):
+    
     def __init__(self):
         super().__init__('fake_receiver')
+        # Sends inputs to the fishhub as if it were the reviver
         self.publisher_command = self.create_publisher(String, '/fish_reciver', 10)
         self.get_logger().info("Receiver listening for binary commands.")
         self.run()
 
     def run(self):
-        while rclpy.ok():
+        while rclpy.ok(): # main loop waits for inputs before sending them to fishhub
                 command_string = ""
-                user_input = input("Enter Command: ").strip().upper()
+                user_input = input("Enter Command: ").strip().upper() # waits for commands letter
                 
                 if user_input == "K":  # Kill command
                     command_string = "111111"
                     self.publish_command(command_string)
                     rclpy.shutdown()
                 else:
-                    command_string = COMMAND_LIST.get(user_input, "X")
-                    if command_string != "X":
+                    command_string = COMMAND_LIST.get(user_input, "X") # returns X if command doesnt exist
+                    if command_string != "X": # checks for bad return before asking for command number
                         num_input = input("Enter Hex Number: ").strip().upper()
                         
-                        if command_string == "R":
+                        if command_string == "R": # handles left right converstion makes them into one unified command from fish persepctive
                             num_input = hex(int(num_input, 16) + 7)[2:].upper()
                         elif command_string == "L":
                             num_input = hex(int(abs(num_input, 16) - 14))[2:].upper()
-
-                        if command_string == "C":
-                                self.publisher_terminal(COMMAND.get(num_input))
                         
-                        number_string = NUMBER_LIST.get(num_input, "X")
-                        if number_string != "X":
+                        number_string = NUMBER_LIST.get(num_input, "X") # returns X if number is bad
+                        if number_string != "X": # If number isnt bad it will save the command into msg and send it to the hub
                             command_string += number_string
                             msg = String()
                             msg.data = command_string
