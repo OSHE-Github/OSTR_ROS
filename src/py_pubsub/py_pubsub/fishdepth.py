fc8c3ae47aa1fd3951937a7170eef0f46ec4fa76
Ike <ikealafita@gmail.com>
2025-04-25 12:41:08 -0400

    Update fishdepth.py

--- src/py_pubsub/py_pubsub/fishdepth.py
+++ src/py_pubsub/py_pubsub/fishdepth.py
@@ -53,54 +53,49 @@ class depthNode(Node):
         self.PWM = OSTR_hat_code.PWMExpander(ADDRPWM, I2C)
         self.motor = OSTR_hat_code.motorDriver(self.PWM, M1P1, M1P2)
         self.adc = OSTR_hat_code.ADC(ADDRADC, I2C)
-       #self.run(0)
-        self.subscription = self.create_subscription(
-            Int32,
-            '/fish_depth',
-            self.command_callback,
-            10)
+       
+        self.subscription = self.create_subscription(Int32,'/fish_depth',self.command_callback,10) # recives commands from fish hub
         self.subscription  # prevent unused variable warning
-        self.subscription_death = self.create_subscription(String, 'death',self.death,10)
+        self.subscription_death = self.create_subscription(String, 'death',self.death,10) # recives kill command
         self.subscription_death #prevent unused variable warning
-        self.local()
+        #self.local() #local method used for locally inputing numbers
         self.get_logger().info("Fish depth module initalized")
-    def local(self):
+    def local(self): #takes local input
         while rclpy.ok():
             n = int(input("number: "))
             n=n*2/3
             self.run(n)
                 
-    def death(self,msg):
+    def death(self,msg): #shuts down when recived
         self.run(0)
         hub_node.destroy_node()
         rclpy.shutdown()
 
 
-    def command_callback(self, msg):
-
+    def command_callback(self, msg): # receives command and turns it into a double between 0 and 10
         depth = msg.data
         depth = depth*2/3
-        if depth > 10:
+        if depth > 10: #ensures it cannot go past breaking point
             depth = 10
         print(depth)
-        self.run(depth)
+        self.run(depth) #sends command to main depth control
 
     def run(self, depth):
-        current = self.adc.read(RESPIN)/16
+        current = self.adc.read(RESPIN)/16 # reads adc to find position
         print(current)
-        if 0 < (current - depth):
-            mode = MDMFB
-            while TOL < (current - depth):
-                self.motor.go(mode, SPEED)
-                current = self.adc.read(RESPIN)/16
+        if 0 < (current - depth): # checks if requested depth is greater than the current position
+            mode = MDMFB # sets motor to fw
+            while TOL < (current - depth): # checks if the distance between depth and current are below the tolerance value
+                self.motor.go(mode, SPEED) # sets motor to move
+                current = self.adc.read(RESPIN)/16 # gets new position value
                 print(current)
         else:
-            mode = MDMRB
-            while TOL < (depth-current):
-                self.motor.go(mode, SPEED)
-                current = self.adc.read(RESPIN)/16
+            mode = MDMRB # sets motor to reverse
+            while TOL < (depth-current): # checks if the distance between depth and current are below the tolerance value
+                self.motor.go(mode, SPEED) # sets motor to move
+                current = self.adc.read(RESPIN)/16 # gets new position value
                 print(current)
-        self.motor.go(mode, 0)
+        self.motor.go(mode, 0) #stops motor
         print("success! "+str(current)+" = " +str(depth))
 
 def main():
